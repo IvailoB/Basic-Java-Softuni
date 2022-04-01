48edbd01067a3977df7e8fdcfa478f9eef062ddb
IvailoB <102227501+IvailoB@users.noreply.github.com>
2022-04-01 15:10:51 +0300

    Add files via upload

---
+++ ForLoopExe/ForLoopExe.java
@@ -0,0 +1,13 @@
+package ForLoopExe;
+
+import java.util.Scanner;
+
+public class ForLoopExe {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 7; i<1000; i+=10){
+            System.out.println(i);
+        }
+    }
+}
