313275d3494fff89f01cc411a8625168ae8d14c6
IvailoB <102227501+IvailoB@users.noreply.github.com>
2022-04-02 13:04:23 +0300

    Add files via upload

---
+++ NastedLoopsLab/Multiplication.java
@@ -0,0 +1,16 @@
+package NastedLoopsLab;
+
+import java.util.Scanner;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int a = 1; a <= 10; a++) {
+            for (int b = 1;b<=10;b++){
+                int sum = a*b;
+                System.out.printf("%d * %d = %d%n",a,b,sum);
+            }
+        }
+    }
+}
